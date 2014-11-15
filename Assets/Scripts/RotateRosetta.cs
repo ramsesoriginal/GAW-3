fda4f0484ae4c4ccee448b3c316bfe38a5bb8c8a
Stefan Insam <ramsesoriginal@gmail.com>
2014-11-15 19:37:14 +0100

    Added rosetta.

---
+++ Assets/Scripts/RotateRosetta.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateRosetta : MonoBehaviour {
+
+	public float rotateSpeedUp = 0;
+	public float rotateSpeedRight = 5;
+	public float rotateSpeedForward = 5;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeedUp, Space.World);
+		transform.Rotate(Vector3.right * Time.deltaTime * rotateSpeedRight, Space.World);
+		transform.Rotate(Vector3.forward * Time.deltaTime * rotateSpeedForward, Space.World);
+	}
+}
